124b7dfd6671cfa3b60534a4c614e06bde879b98
Katelynne Clizer <kateclizer@gmail.com>
2019-09-18 18:09:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,10 @@ end
 introduction("Katelynne")
 
 def introduction_with_language(name,language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
+introduction_with_language("Katelynne", "Ruby")
 
 
 
